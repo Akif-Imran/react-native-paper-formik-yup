722f53cc15659a1002419acb77e6b1ae09d6eb02
Akif-Imran <raiden3322@gmail.com>
2022-11-12 23:20:40 +0500

    Initial setup

---
+++ src/screens/forms/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const MainForm = () => {
+  return (
+    <View>
+      <Text>MainForm</Text>
+    </View>
+  );
+};
+
+export default MainForm;
+
+const styles = StyleSheet.create({});
